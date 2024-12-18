18ebfe5d098650c861d1ab3a18432f741d45432e
noambavli <93512284+noambavli@users.noreply.github.com>
2024-12-19 00:38:42 +0200

    A large list of command options conflicted with the server. All the samples worked individually, but the large list caused it to fail sometimes

--- test_api_depened_commands.py
+++ test_api_depened_commands.py
@@ -4,35 +4,13 @@ import weather_commands
 
 
 def test_weather():
+    #lowercase are intended because all messages converted to lowercase only at first
     commands_examples = [
         'what is the weather in new york',
-        'what is the weather in chicago',
-        'what is the weather in san francisco',
         'what is the weather in london',
         'what is the weather in paris',
         'what is the weather in tokyo',
-        'what is the weather in sydney',
-        'what is the weather in berlin',
-        'what is the weather in madrid',
-        'what is the weather in rome',
-        'what is the weather in seoul',
-        'what is the weather in dubai',
-        'what is the weather in moscow',
-        'what is the weather in beijing',
-        'what is the weather in bangkok',
-        'what is the weather in cape town',
-        'what is the weather in vancouver',
-        'what is the weather in toronto',
-        'what is the weather in mexico city',
-        'what is the weather in los angeles',
-        'what is the weather in barcelona',
-        'what is the weather in amsterdam',
-        'what is the weather in stockholm',
-        'what is the weather in rio de janeiro',
-        'what is the weather in montreal',
-        'what is the weather in melbourne',
-        'what is the weather in toronto',
-        'what is the weather in buenos aires'
+
     ]
 
     for command in commands_examples:
